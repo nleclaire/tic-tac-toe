f1c13f711446c4314de6bf9cf07722bf2be965f3
Noah LeClaire <noahleclaire@gmail.com>
2021-03-17 15:25:47 -0500

    Implementing win conditions

--- script.js
+++ script.js
@@ -8,9 +8,43 @@ const Game = class {
         ];
     }
 
-    turn(id) {
-        this.board[id] = player;
+    turn(target) {
+        if (player == 'X') {
+            target.innerText = 'X';
+        } else {
+            target.innerText = 'O';
+        }
+        this.board[target.id] == 'E' ? this.board[target.id] = player : null;
+        this.isTerminal() ? console.log(this.board[target.id] + " has won the game") : null;
         player == 'X' ? player = 'O' : player = 'X';
+    }
+
+    isTerminal() {
+        // Check rows
+        for (let i = 0; i <= 6; i += 3) {
+            if (this.board[i] !== 'E' && this.board[i + 1] == this.board[i] && this.board[i] == this.board[i + 2]) {
+                document.querySelector('#game-over').classList.add('game-over');
+                return true;
+            }
+        }
+        // Check columns
+        for (let i = 0; i <= 2; i++) {
+            if (this.board[i] !== 'E' && this.board[i + 3] == this.board[i] && this.board[i] == this.board[i + 6]) {
+                document.querySelector('#game-over').classList.add('game-over');
+                return true;
+            }
+        }
+        // Check diagonals
+        for (let i = 0, j = 2; i <= 2; i++, j += 2) {
+            if (this.board[i] !== 'E' && this.board[i] == this.board[i + 4] && this.board[i] == this.board[i + 8]) {
+                document.querySelector('#game-over').classList.add('game-over');
+                return true;
+            }
+            if (this.board[j] !== 'E' && this.board[j] == this.board[j + 2] && this.board[j] == this.board[j + 4]) {
+                document.querySelector('#game-over').classList.add('game-over');
+                return true;
+            }
+        }
     }
 }
 
@@ -22,21 +56,15 @@ let colors = [
     'red', 'green', 'blue',
     'darkred', 'darkgreen', 'darkblue',
     'indianred', 'lawngreen', 'blueviolet'
-]
+];
 
 let boxes = document.querySelectorAll('.square');
 
-colors.forEach((color, index) => boxes[index].style.backgroundColor = color);
-
 boxes.forEach((item) => {
     item.addEventListener('click', (event) => {
-        if (player) {
-            item.innerText = 'X';
-        } else {
-            item.innerText = 'O';
-        }
-        game.turn(event.target.id);
-    })
-})
+        event.target.innerText == "" ? game.turn(event.target) : null;
+        console.log(game.board)
+    });
+});
 
-
+colors.forEach((color, index) => boxes[index].style.backgroundColor = color);
