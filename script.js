fd6196c6c353cbf6148fcd194a5511e60d7ab695
Noah LeClaire <noahleclaire@gmail.com>
2021-03-18 16:47:17 -0500

    Separating State logic

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-const Game = class {
-    constructor(player) {
-        this.player = player;
+const State = class {
+    constructor() {
         this.board = [
             'E', 'E', 'E',
             'E', 'E', 'E',
             'E', 'E', 'E'
-        ];
+        ]
+    }
+
+    // isTerminal checks for win conditions and draws
+    isTerminal() {
+        // Check rows
+        for (let i = 0; i <= 6; i += 3) {
+            if (this.board[i] !== 'E' && this.board[i + 1] == this.board[i] && this.board[i] == this.board[i + 2]) {
+                game.gameOver('win', this.board[i]);
+                return this.board[i];
+            }
+        }
+        // Check columns
+        for (let i = 0; i <= 2; i++) {
+            if (this.board[i] !== 'E' && this.board[i + 3] == this.board[i] && this.board[i] == this.board[i + 6]) {
+                game.gameOver('win', this.board[i]);
+                return true;
+            }
+        }
+        // Check diagonals
+        for (let i = 0, j = 2; i <= 2; i++, j += 2) {
+            if (this.board[i] !== 'E' && this.board[i] == this.board[i + 4] && this.board[i] == this.board[i + 8]) {
+                game.gameOver('win', this.board[i]);
+                return true;
+            }
+            if (this.board[j] !== 'E' && this.board[j] == this.board[j + 2] && this.board[j] == this.board[j + 4]) {
+                game.gameOver('win', this.board[j]);
+                return true;
+            }
+        }
+        // Check for a draw
+        this.board.includes('E') ? null : game.gameOver('draw');
+    }
+}
+
+let state = new State();
+
+const Game = class {
+    constructor(player) {
+        this.player = player;
     }
 
     // Change div text to player value
@@ -16,9 +54,9 @@ const Game = class {
             target.innerText = 'O';
         }
         // update that state
-        this.board[target.id] == 'E' ? this.board[target.id] = player : null;
+        state.board[target.id] == 'E' ? state.board[target.id] = player : null;
         // is the game over?
-        this.isTerminal() ? console.log(this.board[target.id] + " has won the game") : null;
+        state.isTerminal() ? console.log(state.board[target.id] + " has won the game") : null;
         // switch players
         player == 'X' ? player = 'O' : player = 'X';
     }
@@ -28,57 +66,24 @@ const Game = class {
         document.querySelector('#game-over').classList.add('game-over');
         if (type == 'win') {
             document.querySelector('#game-over-text').innerHTML = "" +
-                `<h1>The winner is... ${winner}</h1>` +
+                `<h1 style="text-align: center">The winner is... ${winner}</h1>` +
                 `<div class='reset-button'>Reset board</div>`
         } else {
             document.querySelector('#game-over-text').innerHTML = "" +
                 `<h1>A strange game. <br> <br> The only winning move is not to play.</h1>` +
                 `<div class='reset-button'>Play again?</div>`
-
         }
-        document.querySelector('.reset-button').addEventListener('click', () => game.reset());
-
-    }
-
-    // isTerminal checks for win conditions and cat's games
-    isTerminal() {
-        // Check rows
-        for (let i = 0; i <= 6; i += 3) {
-            if (this.board[i] !== 'E' && this.board[i + 1] == this.board[i] && this.board[i] == this.board[i + 2]) {
-                this.gameOver('win', this.board[i]);
-                return true;
-            }
-        }
-        // Check columns
-        for (let i = 0; i <= 2; i++) {
-            if (this.board[i] !== 'E' && this.board[i + 3] == this.board[i] && this.board[i] == this.board[i + 6]) {
-                this.gameOver('win', this.board[i]);
-                return true;
-            }
-        }
-        // Check diagonals
-        for (let i = 0, j = 2; i <= 2; i++, j += 2) {
-            if (this.board[i] !== 'E' && this.board[i] == this.board[i + 4] && this.board[i] == this.board[i + 8]) {
-                this.gameOver('win', this.board[i]);
-                return true;
-            }
-            if (this.board[j] !== 'E' && this.board[j] == this.board[j + 2] && this.board[j] == this.board[j + 4]) {
-                this.gameOver('win', this.board[j]);
-                return true;
-            }
-        }
-        // Check for a draw
-        this.board.includes('E') ? console.log('not a draw yet') : this.gameOver('draw');
+        document.querySelector('.reset-button').addEventListener('click', () => this.reset());
     }
 
     reset() {
         player = 'X';
         game = new Game('X');
+        state = new State();
         boxes.forEach((item) => {
             item.innerHTML = "";
             item.removeEventListener('click', (event) => {
                 event.target.innerText == "" ? game.turn(event.target) : null;
-                console.log(game.board)
             });
         });
         document.querySelector('#game-over').classList.remove('game-over');
@@ -89,6 +94,42 @@ const Game = class {
 let player = 'X';
 
 let game = new Game(player);
+
+const AI = class {
+    constructor() {
+        this.state = state;
+        this.score = 0;
+    }
+
+    availableNextMoves() {
+        let nextState = [...state.board]
+        console.log(nextState)
+        for (let i = 0; i < this.state.length; i++) {
+            if (nextState[i] == 'E') {
+                nextState[i] = 'O';
+                console.log(nextState);
+                break
+            } else { i++ }
+        }
+
+    }
+
+    isGameOver(board) {
+        if (state.isTerminal() == 'X') {
+            return -10;
+        } else if (state.isTerminal() == 'O') {
+            return 10;
+        } else {
+            return 5;
+        }
+    }
+}
+
+let AIplayer = new AI(game);
+
+AIplayer.availableNextMoves();
+
+
 
 const colors = [
     'red', 'green', 'blue',
@@ -101,7 +142,6 @@ let boxes = document.querySelectorAll('.square');
 boxes.forEach((item) => {
     item.addEventListener('click', (event) => {
         event.target.innerText == "" ? game.turn(event.target) : null;
-        console.log(game.board)
     });
 });
 
